2226bcd42300ca336c0934b925290edbc9164da0
nothing-lin <1353695200@qq.com>
2022-07-31 22:04:09 +0800

    1、导入qq表情资源模块 2、实现消息列表中的来信显示、消息列表下拉刷新

--- app/src/main/java/com/nothinglin/newqitalk/fragment/MessageFragment.java
+++ app/src/main/java/com/nothinglin/newqitalk/fragment/MessageFragment.java
@@ -1,24 +1,123 @@
 package com.nothinglin.newqitalk.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.nothinglin.newqitalk.R;
+import com.nothinglin.newqitalk.adapter.ConversationAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.jpush.im.android.api.JMessageClient;
+import cn.jpush.im.android.api.model.Conversation;
 
 public class MessageFragment extends Fragment {
 
+    //声明组件变量
+    private SwipeRefreshLayout mSRL;//消息列表刷新
+    private ListView mList;//消息列表容器
+    private TextView mTextView;
+
+    private List<Conversation> mData, mConversationList;
+    private ConversationAdapter mAdapter;
+    private Handler handler = new Handler();
+//    private MyRunnable myRunnable = new MyRunnable();
+
+
+    //initview
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_message,container,false);
 
+        //初始化消息列表的组件
+        mSRL = (SwipeRefreshLayout) view.findViewById(R.id.srl_fg1);
+        mList = (ListView) view.findViewById(R.id.lv_fg1);
+        mTextView = (TextView) view.findViewById(R.id.tv_fg1);
+
+        //注册消息菜单的上下文，这样才能体现出列表的功能吧？
+        this.registerForContextMenu(mList);
+
         return view;
     }
+
+    //oncreate方法
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //初始化数据
+        initData();
+
+        //初始化点击监听器
+        initListener();
+    }
+
+    private void initListener() {
+
+        //监听消息刷新列表中的数据
+        mSRL.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mSRL.setRefreshing(true);
+
+                mData.clear();
+                //获取远程服务器中的对话列表
+                //ConversationList是对话列表，Conversation是对话列表item
+                mConversationList = JMessageClient.getConversationList();
+                for (Conversation conversation : mConversationList){
+                    mData.add(conversation);
+                }
+
+                //通知消息更新
+                mAdapter.notifyDataSetChanged();
+                //列表消息状态提示
+                if (mData.size()>0){
+                    mTextView.setText("");
+                }else {
+                    mTextView.setText("没有对话列表消息");
+                }
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        //提示刷新成功
+                        Toast.makeText(getActivity(),"消息列表数据刷新成功",Toast.LENGTH_SHORT).show();
+                        //关闭刷新状态
+                        mSRL.setRefreshing(false);
+                    }
+                },500);
+
+            }
+        });
+
+    }
+
+    private void initData() {
+        //初始化用户数据容器
+        mData = new ArrayList<>();
+        //初始化消息列表容器
+        mConversationList = new ArrayList<>();
+
+        mAdapter = new ConversationAdapter(getActivity(),mData);
+
+        mList.setAdapter(mAdapter);//封装群信息列表到视图中
+
+    }
+
+
+
 }
